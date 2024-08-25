afeb0a0455141e6c94a8208512ca51caa3db1b20
Assaf Bardugo <assaf.bardugo@gmail.com>
2024-08-25 22:02:55 +0300

    initial commit

--- Snake Game/main.c
+++ Snake Game/main.c
@@ -1,14 +1,6 @@
-//
-//  main.c
-//  Snake Game
-//
-//  Created by Assaf Bardugo on 25/08/2024.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
     return 0;
 }
