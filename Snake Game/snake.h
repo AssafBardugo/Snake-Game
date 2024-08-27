f4d485550fe76d74af2940f3a8dd73dd4a47387f
Assaf Bardugo <assaf.bardugo@gmail.com>
2024-08-27 23:42:23 +0300

    Version 1.0

--- Snake Game/snake.h
+++ Snake Game/snake.h
@@ -5,7 +5,27 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
-
 #include <SDL.h>
+
+typedef struct snake Snake;
+
+void initSnake();
+void freeSnake(Snake* to_free);
+
+int snakeLength();
+void increaseSnake();
+void moveSnakeRecursive(Snake* track);
+void moveSnake();
+
+void genApple();
+void detectApple();
+
+void resetSnake();
+void detectCrash();
+
+void renderApple(SDL_Renderer* renderer, int x, int y);
+void renderSnake(SDL_Renderer* renderer, int x, int y);
+void renderGrid(SDL_Renderer* renderer, int x, int y);
+void renderOutline(SDL_Renderer* renderer, int x, int y);
 
 #endif /* SNAKE_H */
